5525fe06b925919304174e2e5280d7032e339955
harsha <harshadhoni436@gmail.com>
2021-02-08 20:57:53 +0530

    Exchandling

---
+++ ExcHandl/src/Exception/Bank.java
@@ -0,0 +1,38 @@
+package Exception;
+import java.util.*;
+public class Bank {
+	class MyException extends Exception 
+	{ 
+	   
+
+		public MyException(String s) {
+			super(s);
+		} 
+	} 
+	public class bank {
+
+		public static void main (String args[]) {
+			int accbal =10000;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Amount to withdraw");
+			int wd = sc.nextInt();
+			
+			try {
+				if(wd<=accbal) {
+					System.out.println("Withdrawn Amount is"+wd);
+					accbal=accbal-wd;
+					System.out.println("Acc Bal="+accbal);
+				}
+				else {
+					throw new MyException("Invalid Amount");
+				}
+			}
+			catch(MyException m) {
+				System.out.println("Caught"); 
+				System.out.println(m.getMessage());
+			}
+		}
+
+	}
+
+}
---
+++ ExcHandl/src/Exception/Divzero.java
@@ -0,0 +1,18 @@
+package Exception;
+import java.util.*;
+public class Divzero {
+	public static void main(String[] args) 
+	{ 
+		int a = 5; 
+		int b = 0; 
+		try { 
+			System.out.println(a / b);
+		} 
+		catch (ArithmeticException e) { 
+			System.out.println( 
+				"Divided by zero cannot possible"); 
+		} 
+	} 
+}
+
+
